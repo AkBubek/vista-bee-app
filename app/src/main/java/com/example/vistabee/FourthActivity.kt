a0338660c064124c15dca687eb8d934e78f45cc5
zhans04Aq <zhans.bazarov@gmail.com>
2024-02-21 17:23:32 +0600

    minor changes

--- app/src/main/java/com/example/vistabee/FourthActivity.kt
+++ app/src/main/java/com/example/vistabee/FourthActivity.kt
@@ -1,11 +1,20 @@
 package com.example.vistabee
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class FourthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fourth)
+        setContentView(R.layout.fourth1)
+
+        val reg_text = findViewById<TextView>(R.id.register_text)
+
+        reg_text.setOnClickListener {
+            val intent = Intent(this, FifthActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
