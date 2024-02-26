8a75c4e9a12d4046a2b4c6c7a1cdd82a03d94fdc
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-02-26 15:56:42 +0500

    added navigation to homepage for both login and signup pages

--- app/src/main/java/com/example/vistabee/FifthActivity.kt
+++ app/src/main/java/com/example/vistabee/FifthActivity.kt
@@ -1,11 +1,28 @@
 package com.example.vistabee
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class FifthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fifth)
+
+        val logTxt = findViewById<TextView>(R.id.login_text)
+
+        logTxt.setOnClickListener{
+            val intent = Intent(this, FourthActivity::class.java)
+            startActivity(intent)
+        }
+
+        val signUpBtn = findViewById<Button>(R.id.sign_up_btn)
+
+        signUpBtn.setOnClickListener{
+            val intent = Intent(this, HomePage::class.java)
+            startActivity(intent)
+        }
     }
 }
