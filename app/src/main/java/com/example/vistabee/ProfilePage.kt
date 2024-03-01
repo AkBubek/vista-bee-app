7ec3aacfdb95f6a9c9266e410a7b219af254cace
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-03-01 22:56:04 +0500

    my changes

--- app/src/main/java/com/example/vistabee/ProfilePage.kt
+++ app/src/main/java/com/example/vistabee/ProfilePage.kt
@@ -1,57 +1,36 @@
 package com.example.vistabee
-
-<<<<<<< HEAD
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
-class ProfilePage : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.seventh)
-        }
-    }
-=======
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-
 class ProfilePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ninth)
 
-        // Инициализируем кнопку по ее идентификатору
         val eduBtn = findViewById<Button>(R.id.educationBtn)
         val expBtn = findViewById<Button>(R.id.experienceBtn)
         val sklBtn = findViewById<Button>(R.id.skillsBtn)
         val sumBtn = findViewById<Button>(R.id.summaryBtn)
         val setBtn = findViewById<ImageView>(R.id.settingBtn)
 
-        // Назначаем обработчик нажатия кнопки
         eduBtn.setOnClickListener {
             val intent = Intent(this, EduPage::class.java)
-            startActivity(intent)
-        }
+            startActivity(intent)         }
         expBtn.setOnClickListener {
             val intent = Intent(this, ExpPage::class.java)
-            startActivity(intent)
-        }
+            startActivity(intent)         }
         sklBtn.setOnClickListener {
             val intent = Intent(this, SkillsPage::class.java)
-            startActivity(intent)
-        }
+            startActivity(intent)         }
         sumBtn.setOnClickListener {
             val intent = Intent(this, SummaryPage::class.java)
-            startActivity(intent)
-        }
+            startActivity(intent)         }
         setBtn.setOnClickListener {
             val intent = Intent(this, SettingPage::class.java)
-            startActivity(intent)
-        }
+            startActivity(intent)}
     }
 }
->>>>>>> faecb00b7da859200cc4122f8fa10854ff9b4418
+
