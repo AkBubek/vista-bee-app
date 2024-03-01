3f0514b13105c46fa7b77eab407f8d8b58ffec13
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-03-01 19:30:45 +0500

    minor changes

--- app/src/main/java/com/example/vistabee/HomePage.kt
+++ app/src/main/java/com/example/vistabee/HomePage.kt
@@ -2,6 +2,7 @@ package com.example.vistabee
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +11,12 @@ class HomePage  : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homepage)
 
+        val profilePicture = findViewById<ImageView>(R.id.profile_picture)
+
+        profilePicture.setOnClickListener{
+            val intent = Intent(this, ProfilePage::class.java)
+            startActivity(intent)
+        }
     }
 }
+
