837d110207548f5d954978dc9dca09f7f1c53399
Akbubek <zakbubek@bk.ru>
2024-03-03 14:56:27 +0600

    backs btns

--- app/src/main/java/com/example/vistabee/SkillsPage.kt
+++ app/src/main/java/com/example/vistabee/SkillsPage.kt
@@ -2,6 +2,7 @@ package com.example.vistabee
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +11,11 @@ class SkillsPage  : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.skills)
 
+        val backSklBtn = findViewById<ImageView>(R.id.sklBackBtn)
+
+        backSklBtn.setOnClickListener {
+            val intent = Intent(this, ProfilePage::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/vistabee/SummaryPage.kt
+++ app/src/main/java/com/example/vistabee/SummaryPage.kt
@@ -2,6 +2,7 @@ package com.example.vistabee
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +11,11 @@ class SummaryPage  : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.summary)
 
+        val backSumBtn = findViewById<ImageView>(R.id.sumBackBtn)
+
+        backSumBtn.setOnClickListener {
+            val intent = Intent(this, ProfilePage::class.java)
+            startActivity(intent)
+        }
     }
 }
