34405e4ecf9ddc91c346b6b2a5f4832db67f9597
zhans04Aq <zhans.bazarov@gmail.com>
2024-03-13 13:15:07 +0600

    some changes

--- app/src/main/java/com/example/vistabee/HomePage.kt
+++ app/src/main/java/com/example/vistabee/HomePage.kt
@@ -13,11 +13,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-
 class HomePage : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
@@ -28,7 +23,7 @@ class HomePage : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         currentUser = firebaseAuth.currentUser ?: return
-        
+
 
         val readBtn = findViewById<Button>(R.id.readmorebtn)
         readBtn.setOnClickListener {
