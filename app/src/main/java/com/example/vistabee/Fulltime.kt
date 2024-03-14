aaf2e5b865e387cf602c5b82ae53c143fc464706
artayxgod <GGGBOXINGTEAMMARATARTAI2006@gmail.com>
2024-03-14 17:07:57 +0500

    stedim otclick button

--- app/src/main/java/com/example/vistabee/Fulltime.kt
+++ app/src/main/java/com/example/vistabee/Fulltime.kt
@@ -1,11 +1,22 @@
 package com.example.vistabee
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class Fulltime : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fulltime)
+
+        val otclickbtn = findViewById<Button>(R.id.button5)
+        otclickbtn.setOnClickListener {
+            val url = "https://wa.me/87023265242?"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
     }
 }
